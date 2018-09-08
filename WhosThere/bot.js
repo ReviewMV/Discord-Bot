a368004294d5d54a0265228d9310c3dcb296a990
Destry Amiott <destry.amiott@gmail.com>
2018-09-08 10:13:01 -0500

    Second Commit

---
+++ WhosThere/bot.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on("ready", ()=> {
+    console.log("I am ready!");
+});
+
+client.on('voiceStateUpdate', (oldMember, newMember) => {
+    let newUserChannel = newMember.voiceChannel;
+    let oldUserChannel = oldMember.voiceChannel;
+
+
+    var channel = client.channels.get('486282607580020787');
+if(oldUserChannel === undefined && newUserChannel !== undefined) {
+    channel.sendMessage(newMember.displayName + ' has joined ' + newMember.voiceChannel, {
+        tts: true
+    });
+
+} else if(newUserChannel === undefined){
+    channel.sendMessage(oldMember.displayName + ' has left ' + oldMember.voiceChannel, {
+        tts: true
+    });
+}
+});
+
+client.login("NDg3Nzk2NTUyMjAzNzYzNzEy.DnS48Q.q2TebKKKcDIOEAvXS-s6dU2MMeo");
